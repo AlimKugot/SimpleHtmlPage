81cfd8d594deb5cc89e0611fa32aec9b659baf67
Alim Kugot <alim.kugotov.ext@zyfra.com>
2023-02-07 18:41:27 +0300

    fixed bug

---
+++ src/main/java/com/zyfra/simplewebapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.zyfra.simplewebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/*")
+    public String getPage() {
+        return "player";
+    }
+}
